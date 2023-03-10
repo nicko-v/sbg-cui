f52efc05a5de652802b0c1020fe88a409d389c23
Nickolay Vasiliev <nicko.v@me.com>
2023-03-10 15:24:45 +0300

    Update sbg_custom_ui.js
    Тест обновления через iOS Shortcuts.

--- sbg_custom_ui.js
+++ sbg_custom_ui.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         SBG CUI
 // @namespace    https://3d.sytes.net/
-// @version      1.0.5
+// @version      1.0.6
 // @downloadURL  https://raw.githubusercontent.com/nicko-v/sbg-cui/main/sbg_custom_ui.js
-// @updateURL  https://raw.githubusercontent.com/nicko-v/sbg-cui/main/sbg_custom_ui.js
+// @updateURL    https://raw.githubusercontent.com/nicko-v/sbg-cui/main/sbg_custom_ui.js
 // @description  SBG Custom UI
 // @author       NV
 // @match        https://3d.sytes.net/
