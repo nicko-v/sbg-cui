82e5396a1733a676b656c7252906a72f0ed6bde4
Nickolay Vasiliev <nicko.v@me.com>
2023-04-21 02:23:23 +0300

    Фикс <style> -> <link>

--- sbg_custom_ui.js
+++ sbg_custom_ui.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SBG CUI
 // @namespace    https://3d.sytes.net/
-// @version      1.2.5
+// @version      1.2.6
 // @downloadURL  https://raw.githubusercontent.com/nicko-v/sbg-cui/main/sbg_custom_ui.js
 // @updateURL    https://raw.githubusercontent.com/nicko-v/sbg-cui/main/sbg_custom_ui.js
 // @description  SBG Custom UI
@@ -18,7 +18,7 @@ async function main() {
 	if (document.querySelector('script[src="/intel.js"]')) { return; }
 
 
-	const USERSCRIPT_VERSION = '1.2.5';
+	const USERSCRIPT_VERSION = '1.2.6';
 	const LATEST_KNOWN_VERSION = '0.2.9';
 	const INVENTORY_LIMIT = 3000;
 	const MIN_FREE_SPACE = 100;
@@ -1317,7 +1317,7 @@ async function main() {
 		let mapFilters = config.mapFilters;
 		let ui = config.ui;
 		let cssVars = document.createElement('style');
-		let styles = document.createElement('style');
+		let styles = document.createElement('link');
 
 		cssVars.innerHTML = (`
       :root {
@@ -1337,7 +1337,8 @@ async function main() {
       }
   `);
 
-		styles.setAttribute('src', 'https://nicko-v.github.io/sbg-cui/styles.css');
+		styles.setAttribute('rel', 'stylesheet');
+		styles.setAttribute('href', 'https://nicko-v.github.io/sbg-cui/styles.css');
 
 		document.head.append(cssVars, styles);
 	}
