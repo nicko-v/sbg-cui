d81818a6a215adf83e227dfa9fe75b6ec07a6223
Nickolay Vasiliev <nicko.v@me.com>
2024-12-29 21:12:39 +0300

    НГ бонусы

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SBG CUI
 // @namespace    https://sbg-game.ru/app/
-// @version      1.14.69
+// @version      1.14.70
 // @downloadURL  https://nicko-v.github.io/sbg-cui/index.min.js
 // @updateURL    https://nicko-v.github.io/sbg-cui/index.min.js
 // @description  SBG Custom UI
@@ -42,11 +42,11 @@
 	window.onerror = (event, source, line, column, error) => { pushMessage([error.message, `Line: ${line}, column: ${column}`]); };
 
 
-	const ACTIONS_REWARDS = { destroy: { region: 125, line: 45, core: 10 } };
+	const ACTIONS_REWARDS = { destroy: { region: 125 * 2, line: 45 * 2, core: 10 * 2 } };
 	const CORES_ENERGY = [0, 500, 750, 1000, 1500, 2000, 2500, 3500, 4000, 5250, 6500];
-	const CORES_LIMITS = [0, 6, 6, 4, 4, 3, 3, 2, 2, 1, 1];
-	const LINES_LIMIT = { out: 30 };
-	const DISCOVERY_COOLDOWN = 90;
+	const CORES_LIMITS = [0, 6, 6, 4, 4, 3, 3, 2, 2, 2, 2];
+	const LINES_LIMIT = { out: 45 };
+	const DISCOVERY_COOLDOWN = 45;
 	const HIGHLEVEL_MARKER = 9;
 	const HIT_TOLERANCE = 15;
 	const HOME_DIR = 'https://nicko-v.github.io/sbg-cui';
@@ -62,7 +62,7 @@
 	const PLAYER_RANGE = 45;
 	const TILE_CACHE_SIZE = 2048;
 	const POSSIBLE_LINES_DISTANCE_LIMIT = 500;
-	const USERSCRIPT_VERSION = '1.14.69';
+	const USERSCRIPT_VERSION = '1.14.70';
 	const VIEW_PADDING = (window.innerHeight / 2) * 0.7;
 	const BLAST_ANIMATION_DURATION = 800;
 
